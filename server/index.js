176de5c73aab7cc40ae5252a410bf9e9c8df0473
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-27 23:48:07 +0700

    (❤️ ω ❤️)

--- server/index.js
+++ server/index.js
@@ -7,17 +7,33 @@ const app = express();
 app.use(bodyParser());
 app.use(cors());
 
+// Posgres client setup
+const {Pool} =require("pg");
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+pgClient.on("connect", (client)=>{
+    client
+    .query("CREATE TABLE IF NOT EXIST values (number INT)")
+    .catch((err)=>console.log(err));
+})
+
 // Redis client setup
 const redis = require("redis");
 const redisClient = redis.createClient({
     host: keys.redisHost,
     port: keys.redisPort,
-    retry_strategy: ()=>1000
+    retry_strategy: () => 1000
 });
 
 const redisPublisher = redisClient.duplicate();
 
 // Express routes handler
-app.listen(5000, (err)=>{
+app.listen(5000, (err) => {
     console.log("Listening");
 })
